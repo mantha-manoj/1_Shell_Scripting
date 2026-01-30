e64cc529ca3a222b2fae99ebb7ea97b11e9f0eee
manoj515876 <manojputtaluckey@gmail.com>
2026-01-31 01:23:06 +0530

    Output storing in a variable

--- 06-store_ouput_variable.sh
+++ 06-store_ouput_variable.sh
@@ -1,5 +1,12 @@
 #!/bin/bash 
 
-START_TIME=$(date)
+START_TIME=$(date +%s)
 
 echo "Script execute at : $START_TIME"
+
+sleep 10
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(($END_TIME-$START_TIME))
+
+echo "Script executed in: $TOTAL_TIME seconds"
