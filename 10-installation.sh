0b03b8e5230145711a897b7867dda0cfc79ac11c
manoj515876 <manojputtaluckey@gmail.com>
2026-01-31 01:37:41 +0530

    Installing Nginx

---
+++ 10-installation.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+
+if [ $USER_ID -ne 0 ]; then
+    echo "Please run this script with root user access"
+    exit 1
+fi
+
+echo "Installing Nginx"
+
+dnf install nginx -y
+
+if [ $? -ne 0 ]; then
+    echo "Installing Nginx ... FAILURE"
+    exit 1
+else 
+    echo "Installing Nginx ... SUCCESS"
+fi
