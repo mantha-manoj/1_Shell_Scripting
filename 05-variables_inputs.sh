452aecfc6c45119e8b843fd9fef191618266e20c
manoj515876 <manojputtaluckey@gmail.com>
2026-01-31 01:15:41 +0530

    Passing inputs to variables

---
+++ 05-variables_inputs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Please Enter your username : "
+read USER_NAME
+
+echo "User name is $USER_NAME"
+
+echo "Please Enter your password : "
+read -s PASSWORD
+
+echo "Password is $PASSWORD"
