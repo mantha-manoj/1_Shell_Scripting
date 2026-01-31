2fe5f3f3584cfa0f547b7b02d098666e0d17e0fd
manoj515876 <manojputtaluckey@gmail.com>
2026-01-31 19:20:49 +0530

    package Installing using for loop

--- 15-loops.sh
+++ 15-loops.sh
@@ -22,7 +22,7 @@ VALIDATE() {
 
 for package in $@
 do 
-    dnf list installed $package $>> $LOGS_FILE
+    dnf list installed $package &>> $LOGS_FILE
     if [ $? -ne 0 ]; then
         echo "$package not installed, installing now"
         dnf install $package -y &>>$LOGS_FILE
