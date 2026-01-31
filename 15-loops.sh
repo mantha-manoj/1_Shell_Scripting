b4f1cbd1d4cc99feeb917504ae33b6d3817b82fc
manoj515876 <manojputtaluckey@gmail.com>
2026-01-31 19:16:11 +0530

    package Installing using for loop

--- 15-loops.sh
+++ 15-loops.sh
@@ -4,7 +4,7 @@ USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-script"
 LOGS_FILE="/var/log/shell-script/$0.log"
 
-if [ $USER_ID -ne 0 ]; then
+if [ $USERID -ne 0 ]; then
     echo "Please run this script with root user access" | tee -a $LOGS_FILE
     exit 1
 fi 
